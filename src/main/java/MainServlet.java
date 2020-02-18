4e16b983897c964482e5f02f6a4cba610c1fe60d
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-18 13:55:55 +0000

    back

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -70,7 +70,7 @@ public class MainServlet extends HttpServlet {
                case 13: 
 
                     JSONObject jsonToReturn13 = new JSONObject();
-                    jsonToReturn13.put("answer", "blue");
+                    jsonToReturn13.put("answer", "red");
                     out.println(jsonToReturn13.toString());
 
                     break; 
